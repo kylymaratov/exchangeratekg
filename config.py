2b6c4251e856bf9936d4381667fc79aad04d58ab
kylym <kylym.maratovv@gmail.com>
2023-02-27 20:21:15 +0600

    remove token in config, add alternative method

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-TOKEN = "5800761441:AAGjJ1utujXvVt2R9Ej-BFSRfM8FaZm8BDY"
+import sys
+
+TOKEN = None
+
+try:
+    r_file = open("token.txt", "r")
+    TOKEN = r_file.read()
+except:
+    TOKEN = input("Peste bot token: ")
+    file = open("token.txt", "a")
+    file.write(TOKEN)
+    file.close()
